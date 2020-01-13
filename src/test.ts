8ffe9c713fc074265b1236d0a7c2955969b4eabd
Mislav Marohnić <mislav.marohnic@gmail.com>
2020-01-13 17:28:26 +0100

    Recognize version string in GitHub download URLs
    Fixes #2

--- src/test.ts
+++ src/test.ts
@@ -6,6 +6,12 @@ test('version.fromUrl()', t => {
     fromUrl('https://github.com/me/myproject/archive/v1.2.3.tar.gz'),
     'v1.2.3'
   )
+  t.is(
+    fromUrl(
+      'https://github.com/me/myproject/releases/download/v1.2.3/file.tgz'
+    ),
+    'v1.2.3'
+  )
   t.is(fromUrl('http://myproject.net/download/v1.2.3.tgz'), 'v1.2.3')
   t.is(fromUrl('https://example.com/v1.2.3.zip'), 'v1.2.3')
 })
